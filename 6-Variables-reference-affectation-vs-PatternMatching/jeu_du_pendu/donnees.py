674fa276a16e57f718a6996d3ff890620293841c
erfaliel <fulielas@gmail.com>
2018-05-26 21:48:25 +0200

    fichier de donnees.py pour le jeu de pendu

---
+++ 6-Variables-reference-affectation-vs-PatternMatching/jeu_du_pendu/donnees.py
@@ -0,0 +1,35 @@
+# Paramètre du nombre de coup avant que je joeur ne perde
+count = 8
+
+# Liste des mots à trouver, proposés par le jeu.
+game_list = [
+  'nez',
+  'jet',
+  'jeu',
+  'zoo',
+  'fer',
+  'dos',
+  'riz',
+  'tir',
+  'emu',
+  'abus',
+  'ados',
+  'banc',
+  'bile',
+  'brut',
+  'brai',
+  'chat',
+  'demo',
+  'deco',
+  'dodu',
+  'aimer',
+  'verge',
+  'video',
+  'vitre',
+  'abusif',
+  'boueux',
+  'addittif',
+  'adeptes',
+  'abritais',
+  'assomant'
+]
