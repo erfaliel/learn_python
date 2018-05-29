002806491852b263e81a11e077867bc6770ef84d
erfaliel <fulielas@gmail.com>
2018-05-29 18:04:22 +0200

    Première version du jeu de pendu

---
+++ 6-Variables-reference-affectation-vs-PatternMatching/jeu_du_pendu/fonctions.py
@@ -0,0 +1,43 @@
+import random
+
+def generate_word_to_find(game_list):
+  """Take a list and return a tuple.
+  from a list, extract a word by random fonction, change this word into a list and the number of
+  the char for this one."""
+
+  word_to_find_char = random.choice(game_list)
+  number_word_to_find_int = len(word_to_find_char)
+  word_to_find_list = [char for char in word_to_find_char]
+  return (word_to_find_list, number_word_to_find_int)
+
+
+def check_char(searched_word_list, word_to_find_list, user_char):
+  """Take two list and a char and return the modified first list.
+  the fonction allowed to check if user_char is in word_to_find_list.
+  So it sets the char in the correct slot of the searched_word_list. """
+
+  for (index, element) in enumerate(word_to_find_list):
+    if element == user_char :
+      searched_word_list[index] = user_char
+  return searched_word_list
+
+def word_user_init(number_word_to_find_int):
+  """Take an integer and return a list.
+  form integer, generate a list of '*'. """ 
+
+  searched_word_list = list()
+  i = 0
+  while i < number_word_to_find_int :
+    searched_word_list.append('*')
+    i += 1
+  return searched_word_list
+
+# Unit tests 
+if __name__ == "__main__" :
+  game_list = ['toto', 'otot']
+  (word_to_find_list, number_word_to_find_int) = generate_word_to_find(game_list)
+  print(word_to_find_list)
+  print(number_word_to_find_int)
+  searched_word_list = word_user_init(number_word_to_find_int)
+  print(searched_word_list)
+  print(check_char(searched_word_list, word_to_find_list, 'o'))
---
+++ 6-Variables-reference-affectation-vs-PatternMatching/jeu_du_pendu/pendu.py
@@ -0,0 +1,7 @@
+import donnees
+import fonctions
+
+word_to_find_list, nb = fonctions.generate_word_to_find(donnees.game_list)
+print(word_to_find_list, ' ', nb)
+
+
