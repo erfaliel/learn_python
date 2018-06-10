7e3f80419afd8d9b0b77bff1efb01aff70d971d6
erfaliel <fulielas@gmail.com>
2018-06-10 19:49:59 +0200

    add players

--- 6-Variables-reference-affectation-vs-PatternMatching/jeu_du_pendu/pendu.py
+++ 6-Variables-reference-affectation-vs-PatternMatching/jeu_du_pendu/pendu.py
@@ -1,12 +1,19 @@
 import donnees
 from fonctions import *
 
+#Get randomly a word in the list
 word_to_find_list, nb = generate_word_to_find(donnees.game_list)
 print(word_to_find_list, ' ', nb)
+# Generate word to find with *
 searched_word_list = word_user_init(nb)
 print(searched_word_list)
+# Init games
 tries_int = 1
 win_bool = False
+# get the name of the current player
+name_string = input("Veuillez saisir votre nom : ")
+# check player
+# if player exist in the struct score then "votre score actuel est de" else "Bienvenue"
 
 while tries_int <= donnees.count and win_bool is False :
   user_char = getchar()
